eaae422c26528c27d575fe78c688eb7d66e65bbc
jinrong <hjr2027@163.com>
2020-09-21 15:52:48 +0800

    finish 1-4

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let res = 0;
+  collection.forEach(e => {
+    if (/^粤A/.test(e)) {
+      res += 1;
+    }
+  });
+  return res;
 }
--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,12 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const res = collection.filter(item => item % 3 === 0);
+  return res;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return Array.from(new Set(collection));
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
